c969577187d42e038767735775f7c142e971a3c0
Alexander Ignatov <aleksandar.ignatov@smule.com>
2023-04-21 22:31:34 +0300

    Initial commit

---
+++ BDZDelays/BDZDelaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BDZDelaysApp.swift
+//  BDZDelays
+//
+//  Created by Alexander Ignatov on 21.04.23.
+//
+
+import SwiftUI
+
+@main
+struct BDZDelaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
