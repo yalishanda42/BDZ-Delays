d321c5fe7036238ffdb6b01229c7127a9b759402
Alexander Ignatov <aleksandar.ignatov@smule.com>
2023-04-30 21:17:55 +0300

    Improve blinking style

--- BDZDelays/Features/Train/TrainView.swift
+++ BDZDelays/Features/Train/TrainView.swift
@@ -110,14 +110,16 @@ private struct DisplayTimeView: View {
 // MARK: - Blinking
 
 fileprivate struct BlinkViewModifier: ViewModifier {
-    
     let duration: Double
     @State private var opacity: Double = 0.0
     
     func body(content: Content) -> some View {
         content
             .opacity(opacity)
-            .animation(.easeOut(duration: duration).repeatForever(), value: opacity)
+            .animation(
+                .easeInOut(duration: duration).repeatForever(),
+                value: opacity
+            )
             .onAppear {
                 withAnimation {
                     opacity = 1.0
@@ -127,7 +129,7 @@ fileprivate struct BlinkViewModifier: ViewModifier {
 }
 
 fileprivate extension View {
-    func blinking(duration: Double = 0.75) -> some View {
+    func blinking(duration: Double = 1.0) -> some View {
         modifier(BlinkViewModifier(duration: duration))
     }
 }
