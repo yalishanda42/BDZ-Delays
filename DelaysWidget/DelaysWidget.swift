b37ae1546b756b9f4d967fdb3c9858edffb2edc0
Alexander Ignatov <yalishanda@abv.bg>
2023-06-07 21:54:46 +0300

    Update correctly

--- DelaysWidget/DelaysWidget.swift
+++ DelaysWidget/DelaysWidget.swift
@@ -9,11 +9,19 @@ import WidgetKit
 import SwiftUI
 import Intents
 
+import Dependencies
+
+import SharedModels
+import StationRepository
+import StationRepositoryLive
+
 struct Provider: IntentTimelineProvider {
     typealias Entry = SimpleEntry
     
+    @Dependency(\.stationRepository) var repository
+    
     func placeholder(in context: Context) -> Entry {
-        Entry(date: Date(), configuration: SelectStationIntent())
+        Entry(date: Date(), configuration: SelectStationIntent(), result: .success(prevewiewData))
     }
 
     func getSnapshot(
@@ -21,7 +29,11 @@ struct Provider: IntentTimelineProvider {
         in context: Context,
         completion: @escaping (Entry) -> ()
     ) {
-        let entry = Entry(date: Date(), configuration: configuration)
+        let entry = Entry(
+            date: Date(),
+            configuration: configuration,
+            result: .success(prevewiewData)
+        )
         completion(entry)
     }
 
@@ -30,21 +42,38 @@ struct Provider: IntentTimelineProvider {
         in context: Context,
         completion: @escaping (Timeline<Entry>) -> ()
     ) {
-        let entries: [Entry] = (0 ..< 30).map {  // nxt half an hour
-            let now = Date()
-            let entryDate = Calendar.current.date(byAdding: .minute, value: $0, to: now)!
-            let entry = Entry(date: entryDate, configuration: configuration)
-            return entry
+        Task {
+            let trainsResult: Result<[TrainAtStation], Error>?
+            
+            defer {
+                let now = Date()
+                let entry = Entry(date: now, configuration: configuration, result: trainsResult)
+                let refreshDate = Calendar.current.date(byAdding: .minute, value: 1, to: now)!
+                let timeline = Timeline(entries: [entry], policy: .after(refreshDate))
+                completion(timeline)
+            }
+            
+            guard
+                let name = configuration.station,
+                let station = BGStation(name: name)
+            else {
+                trainsResult = nil
+                return
+            }
+            
+            do {
+                trainsResult = .success(try await repository.fetchTrainsAtStation(station))
+            } catch let error {
+                trainsResult = .failure(error)
+            }
         }
-        
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
     }
 }
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
     let configuration: SelectStationIntent
+    let result: Result<[TrainAtStation], Error>?
 }
 
 struct DelaysWidget: Widget {
@@ -54,7 +83,57 @@ struct DelaysWidget: Widget {
         IntentConfiguration(kind: kind, intent: SelectStationIntent.self, provider: Provider()) { entry in
             WidgetView(entry: entry)
         }
-        .configurationDisplayName("Delays Widget")
-        .description("This is the widget of the BDZ Delays app.")
+        .configurationDisplayName("БДЖ Закъснения")
+        .description("Тази джаджа ви показва движението на влаковете от/към избрана гара в реално време за следващите 6 часа.")
     }
 }
+
+fileprivate let prevewiewData: [TrainAtStation] = [
+    .init(
+        number: TrainNumber(
+            type: .suburban,
+            number: 2112
+        ),
+        from: .bulgarian(.sofia),
+        to: .bulgarian(.varna),
+        schedule: .departureOnly(Date(timeIntervalSince1970: 3600)),
+        delay: nil,
+        movement: .leavingStation
+    ),
+    .init(
+        number: TrainNumber(
+            type: .suburban,
+            number: 21123
+        ),
+        from: .bulgarian(.sofia),
+        to: .bulgarian(.burgas),
+        schedule: .departureOnly(Date(timeIntervalSince1970: 3600)),
+        delay: .seconds(240),
+        movement: .leavingStation
+    ),
+    .init(
+        number: TrainNumber(
+            type: .fast,
+            number: 2212
+        ),
+        from: .bulgarian(.sofia),
+        to: .bulgarian(.vraca),
+        schedule: .arrivalOnly(Date(timeIntervalSince1970: 3600)),
+        delay: .seconds(240),
+        movement: .inOperation
+    ),
+    .init(
+        number: TrainNumber(
+            type: .normal,
+            number: 2312
+        ),
+        from: .bulgarian(.plovdiv),
+        to: .bulgarian(.sofia),
+        schedule: .full(
+            arrival: Date(timeIntervalSince1970: 3600),
+            departure: Date(timeIntervalSince1970: 3660)
+        ),
+        delay: nil,
+        movement: .notYetOperating
+    ),
+]
--- DelaysWidget/WidgetView.swift
+++ DelaysWidget/WidgetView.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 import WidgetKit
 
+import SharedModels
+
+// MARK: - View
+
 struct WidgetView: View {
     let entry: Provider.Entry
     
     @Environment(\.widgetFamily) private var size: WidgetFamily
-
+    
     @ViewBuilder
     var body: some View {
         VStack(spacing: 1) {
@@ -24,27 +28,26 @@ struct WidgetView: View {
                 } else {
                     Text("София").redacted(reason: .placeholder)
                 }
-            }.frame(maxHeight: 28)
-            
-            ForEach(Array(1...rows), id: \.self) { _ in
-                VStack(spacing: 0) {
-                    HStack {
-                        Text("БВ 2612")
-                        if size != .systemSmall {
-                            Text("Видин пътн. - Горна оряховица")
-                        }
-                        Spacer()
-                        Text("≈ 11:22 / 11:32")
-                    }
-                    Divider()
-                }
-                .lineLimit(1)
-                .font(.footnote)
-                .padding(.horizontal, 3)
             }
+            .frame(maxHeight: 28)
+            .bold()
+            .foregroundColor(.black)
             
-            Spacer()
-        }.padding(.bottom, 10)
+            switch entry.result {
+            case let .some(.success(trains)) where trains.isEmpty:
+                Text("Няма влакове 6 часа напред.")
+            case let .some(.success(trains)):
+                list(of: trains)
+                Spacer()
+            case .some(.failure):
+                Text("Неуспешна връзка.")
+            case .none:
+                list(of: placeholders)
+                    .redacted(reason: .placeholder)
+                Spacer()
+            }
+        }
+        .padding(.bottom, 10)
     }
     
     private var rows: Int {
@@ -55,7 +58,116 @@ struct WidgetView: View {
             return 7
         }
     }
+    
+    @ViewBuilder
+    private func list(of trains: [TrainAtStation]) -> some View {
+        ForEach(Array(zip(0..<rows, trains)), id: \.0) { (_, train) in
+            VStack(spacing: 0) {
+                HStack {
+                    Text("\(train.number.type.abbrev) \(String(train.number.number))").bold()
+                    if size != .systemSmall {
+                        Text("\(train.from.name) - \(train.to.name)")
+                    }
+                    Spacer()
+                    schedule(of: train).appliedStatusColor(train)
+                }
+                Divider()
+            }
+            .lineLimit(1)
+            .font(.system(size: 10))
+            .padding(.horizontal, 3)
+        }
+    }
+    
+    @ViewBuilder
+    private func schedule(of train: TrainAtStation) -> some View {
+        Group {
+            if train.delay != nil && [TrainAtStation.MovementState](arrayLiteral: .inOperation, .doorsOpen, .notYetOperating).contains(train.movement)  {
+                Text("≈")
+            }
+            
+            switch train.schedule {
+            case let .arrivalOnly(arrival):
+                Text("\(formatted(date: arrival, addedDelay: train.delay)) / -----")
+            case let .departureOnly(departure):
+                Text("----- / \(formatted(date: departure, addedDelay: train.delay))")
+            case let .full(arrival: arrival, departure: departure):
+                Text("\(formatted(date: arrival, addedDelay: train.delay)) / \(formatted(date: departure, addedDelay: train.delay))")
+            }
+        }
+    }
+    
+    private func formatted(date: Date, addedDelay: Duration? = nil) -> String {
+        let actual = addedDelay.map {
+            date.addingDuration($0)
+        } ?? date
+        
+        return actual.hoursAndMinutes
+    }
+    
+    private var placeholders: [TrainAtStation] {
+        Array(
+            repeating: TrainAtStation(
+                number: .init(type: .fast, number: 2112),
+                from: .bulgarian(.sofia),
+                to: .bulgarian(.varna),
+                schedule: .full(arrival: Date(), departure: Date()),
+                delay: nil,
+                movement: .notYetOperating
+            ),
+            count: rows
+        )
+    }
 }
+
+fileprivate extension View {
+    @ViewBuilder
+    func appliedStatusColor(_ data: TrainAtStation) -> some View {
+        switch data.movement {
+        case .doorsOpen:
+            foregroundColor(.purple)
+        case .inOperation:
+            if data.delay == nil {
+                foregroundColor(.green)
+            } else {
+                foregroundColor(.red)
+            }
+        case .leavingStation, .stopped:
+            foregroundColor(.gray)
+        case .notYetOperating:
+            self
+        }
+    }
+}
+
+// MARK: - Conversions
+
+fileprivate extension Duration {
+    var minutes: Int {
+        Int(components.seconds / 60)
+    }
+}
+
+fileprivate extension Date {
+    func addingDuration(_ duration: Duration) -> Date {
+        self + TimeInterval(duration.components.seconds)
+    }
+}
+
+fileprivate let dateFormatter: DateFormatter = {
+    let result = DateFormatter()
+    result.dateFormat = "HH:mm"
+    result.timeZone = TimeZone(identifier: "Europe/Sofia")
+    return result
+}()
+
+fileprivate extension Date {
+    var hoursAndMinutes: String {
+        dateFormatter.string(from: self)
+    }
+}
+
+// MARK: - Previews
 
 struct WidgetView_Previews: PreviewProvider {
     private static let sizes: [(WidgetFamily, String)] = [
@@ -67,7 +179,11 @@ struct WidgetView_Previews: PreviewProvider {
     
     static var previews: some View {
         ForEach(sizes, id: \.1) {
-            WidgetView(entry: SimpleEntry(date: Date(), configuration: SelectStationIntent()))
+            WidgetView(entry: SimpleEntry(
+                date: Date(),
+                configuration: SelectStationIntent(),
+                result: nil
+            ))
                 .previewContext(WidgetPreviewContext(family: $0.0))
                 .previewDisplayName($0.1)
         }
