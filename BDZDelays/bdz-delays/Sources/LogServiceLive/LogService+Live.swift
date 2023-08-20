75557c2eff6ecca5c0430f6dbbcd9e3242425780
Alexander Ignatov <yalishanda@abv.bg>
2023-08-20 04:11:30 +0300

    Enable Firebase logging

--- BDZDelays/bdz-delays/Sources/LogServiceLive/LogService+Live.swift
+++ BDZDelays/bdz-delays/Sources/LogServiceLive/LogService+Live.swift
@@ -4,5 +4,6 @@ import Dependencies
 extension LogService: DependencyKey {
     public static let liveValue = Self(providers: [
         StandardLoggingProvider(),
+        FirebaseLoggingProvider(),
     ])
 }
